d3e762bf2183a32b996fb05546cb3352c5d19e31
Khang <khangphan1310@gmail.com>
2025-10-23 15:37:48 -0700

    2nd

--- MP1.py
+++ MP1.py
@@ -1,5 +1,7 @@
 from socket import *
 import os
+from datetime import datetime
+from email.utils import formatdate
 
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -8,8 +10,9 @@ serverSocket.listen(1)
 print('The server is ready to receive')
 
 while True:
+    print("Waiting for a new connection...")
     connectionSocket, addr = serverSocket.accept()
-    print(f'Connection from {addr}')
+    print(f'Connection established with {addr}')
 
     request = connectionSocket.recv(1024).decode()
     print("Request received:\n", request)
@@ -18,13 +21,20 @@ while True:
         request_line = request.split('\n')[0]
         method, path, version = request_line.split()
     except ValueError:
+        print("Status code: 400")
         connectionSocket.send(b"HTTP/1.1 400 Bad Request\r\n\r\n")
         connectionSocket.close()
         continue
 
     # Only allow GET requests for now
     if method != 'GET':
-        response = "HTTP/1.1 403 Forbidden\r\n\r\n"
+        print("Status code: 403")
+        response = (
+            "HTTP/1.1 403 Forbidden\r\n"
+            "Content-Type: text/html\r\n"
+            "Connection: close\r\n\r\n"
+            "<html><body><h1>403 Forbidden</h1></body></html>"
+        )
         connectionSocket.send(response.encode())
         connectionSocket.close()
         continue
@@ -33,6 +43,19 @@ while True:
     filename = path.strip('/')
     if filename == '':
         filename = 'index.html'  # default file
+    
+    # Enforce 403 Forbidden for files with "secret" in the filename
+    if "secret" in filename:
+        print("Status code: 403")
+        response = (
+            "HTTP/1.1 403 Forbidden\r\n"
+            "Content-Type: text/html\r\n"
+            "Connection: close\r\n\r\n"
+            "<html><body><h1>403 Forbidden</h1></body></html>"
+        )
+        connectionSocket.send(response.encode())
+        connectionSocket.close()
+        continue
 
     # File existence check
     if os.path.exists(filename):
@@ -41,10 +64,19 @@ while True:
         response = (
             "HTTP/1.1 200 OK\r\n"
             f"Content-Length: {len(body)}\r\n"
-            "Content-Type: text/html\r\n\r\n"
+            "Content-Type: text/html\r\n"
+            f"Last-Modified: {formatdate(os.path.getmtime(filename), usegmt=True)}\r\n"
+            "Connection: close\r\n\r\n"
         ).encode() + body
+        print("Status code: 200")
     else:
-        response = "HTTP/1.1 404 Not Found\r\n\r\nFile not found".encode()
+        print("Status code: 404")
+        response = (
+            "HTTP/1.1 404 Not Found\r\n"
+            "Content-Type: text/html\r\n"
+            "Connection: close\r\n\r\n"
+            "<html><body><h1>404 Not Found</h1></body></html>"
+        ).encode()
 
     connectionSocket.send(response)
     connectionSocket.close()
