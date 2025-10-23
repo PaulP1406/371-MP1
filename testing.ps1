8549c79b1c1f2a688708e827c9472c1465d06510
PaulP1406 <ducduy0308@gmail.com>
2025-10-23 16:12:10 -0700

    updates

--- testing.ps1
+++ testing.ps1
@@ -1,19 +1,19 @@
 Write-Host "===== Test 200 OK ====="
-curl.exe -v http://127.0.0.1:8080/test.html
+curl.exe -v http://127.0.0.1:12000/test.html
 Write-Host ""
 
 Write-Host "===== Test 403 Forbidden ====="
-curl.exe -v http://127.0.0.1:8080/testForbidden.html
+curl.exe -v http://127.0.0.1:12000/testForbidden.html
 Write-Host ""
 
 Write-Host "===== Test 404 Not Found ====="
-curl.exe -v http://127.0.0.1:8080/doesNotExist.html
+curl.exe -v http://127.0.0.1:12000/doesNotExist.html
 Write-Host ""
 
 Write-Host "===== Test 304 Not Modified ====="
-curl.exe -v -H "If-Modified-Since: Thu, 01 Jan 2099 00:00:00 GMT" http://127.0.0.1:8080/test.html
+curl.exe -v -H "If-Modified-Since: Thu, 01 Jan 2099 00:00:00 GMT" http://127.0.0.1:12000/test.html
 Write-Host ""
 
 Write-Host "===== Test 400 Bad Request ====="
-curl.exe -v -X "BADMETHOD" http://127.0.0.1:8080/test.html
+curl.exe -v -X "BADMETHOD" http://127.0.0.1:12000/test.html
 Write-Host ""
