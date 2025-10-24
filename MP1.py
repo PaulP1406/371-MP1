69196d31522be3ad7ccac00a1e8604229fd62717
Khang <khangphan1310@gmail.com>
2025-10-23 22:54:31 -0700

    304 working

--- MP1.py
+++ MP1.py
@@ -1,5 +1,7 @@
 from socket import *
 import os
+from datetime import datetime, timezone
+from email.utils import parsedate_to_datetime
 
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -15,23 +17,50 @@ while True:
 
     # Handle the 200 case
     fileName = request.split('\n')[0].split()[1].strip('/')
+
+    # Extract "If-Modified-Since" header if present
+    if_modified_since = None
+    for line in request.split('\n'):
+        if line.lower().startswith("if-modified-since:"):
+            try:
+                if_modified_since = parsedate_to_datetime(line.split(":", 1)[1].strip())
+                # Ensure timezone-aware in UTC
+                if if_modified_since.tzinfo is None:
+                    if_modified_since = if_modified_since.replace(tzinfo=timezone.utc)
+                else:
+                    if_modified_since = if_modified_since.astimezone(timezone.utc)
+            except Exception:
+                if_modified_since = None
+
     if os.path.exists(fileName) and os.access(fileName, os.R_OK):
-        try:
-            with open(fileName, 'rb') as f:
-                body = f.read()
+        # Get the file's last modified time (timezone-aware UTC)
+        file_mtime = datetime.fromtimestamp(os.path.getmtime(fileName), tz=timezone.utc)
+
+        # Handle the 304 case (client has cached version)
+        if if_modified_since and (file_mtime - if_modified_since).total_seconds() < 3:
             response = (
-                "HTTP/1.1 200 OK\r\n"
-                "Content-Type: text/html\r\n\r\n"
-                f"Content-Length: {len(body)}\r\n"
-            ).encode() + body
-        except PermissionError:
-            # In case access changes while reading, send 403 instead of crashing
-            response = (
-                "HTTP/1.1 403 Forbidden\r\n"
+                "HTTP/1.1 304 Not Modified\r\n"
                 "Content-Type: text/html\r\n"
                 "\r\n"
-                "<html><body><h1>403 Forbidden</h1><p>You do not have permission to access this file.</p></body></html>"
             ).encode()
+        else:
+            try:
+                with open(fileName, 'rb') as f:
+                    body = f.read()
+                response = (
+                    "HTTP/1.1 200 OK\r\n"
+                    "Content-Type: text/html\r\n"
+                    f"Content-Length: {len(body)}\r\n"
+                    f"Last-Modified: {file_mtime.strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
+                    "\r\n"
+                ).encode() + body
+            except PermissionError:
+                response = (
+                    "HTTP/1.1 403 Forbidden\r\n"
+                    "Content-Type: text/html\r\n"
+                    "\r\n"
+                    "<html><body><h1>403 Forbidden</h1><p>You do not have permission to access this file.</p></body></html>"
+                ).encode()
 
     # Handle the 403 case (Set the testForbidden.html permission to not accessible similar to chmod)
     elif os.path.exists(fileName) and not os.access(fileName, os.R_OK):
