5cd4febab7e374a5bc8fb1aa4d30f82a5f1ab7ea
Khang <khangphan1310@gmail.com>
2025-10-23 22:38:57 -0700

    200 403 404 working

--- MP1.py
+++ MP1.py
@@ -1,7 +1,5 @@
 from socket import *
 import os
-from datetime import datetime
-from email.utils import formatdate
 
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
@@ -10,72 +8,47 @@ serverSocket.listen(1)
 print('The server is ready to receive')
 
 while True:
-    print("Waiting for a new connection...")
     connectionSocket, addr = serverSocket.accept()
-    print(f'Connection established with {addr}')
 
     request = connectionSocket.recv(1024).decode()
     print("Request received:\n", request)
 
-    try:
-        request_line = request.split('\n')[0]
-        method, path, version = request_line.split()
-    except ValueError:
-        print("Status code: 400")
-        connectionSocket.send(b"HTTP/1.1 400 Bad Request\r\n\r\n")
-        connectionSocket.close()
-        continue
+    # Handle the 200 case
+    fileName = request.split('\n')[0].split()[1].strip('/')
+    if os.path.exists(fileName) and os.access(fileName, os.R_OK):
+        try:
+            with open(fileName, 'rb') as f:
+                body = f.read()
+            response = (
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: text/html\r\n\r\n"
+                f"Content-Length: {len(body)}\r\n"
+            ).encode() + body
+        except PermissionError:
+            # In case access changes while reading, send 403 instead of crashing
+            response = (
+                "HTTP/1.1 403 Forbidden\r\n"
+                "Content-Type: text/html\r\n"
+                "\r\n"
+                "<html><body><h1>403 Forbidden</h1><p>You do not have permission to access this file.</p></body></html>"
+            ).encode()
 
-    # Only allow GET requests for now
-    if method != 'GET':
-        print("Status code: 403")
+    # Handle the 403 case (Set the testForbidden.html permission to not accessible similar to chmod)
+    elif os.path.exists(fileName) and not os.access(fileName, os.R_OK):
         response = (
             "HTTP/1.1 403 Forbidden\r\n"
             "Content-Type: text/html\r\n"
-            "Connection: close\r\n\r\n"
-            "<html><body><h1>403 Forbidden</h1></body></html>"
-        )
-        connectionSocket.send(response.encode())
-        connectionSocket.close()
-        continue
+            "\r\n"
+            "<html><body><h1>403 Forbidden</h1><p>You do not have permission to access this file.</p></body></html>"
+        ).encode()
 
-    # Remove leading '/' from path
-    filename = path.strip('/')
-    if filename == '':
-        filename = 'index.html'  # default file
-    
-    # Enforce 403 Forbidden for files with "secret" in the filename
-    if "secret" in filename:
-        print("Status code: 403")
-        response = (
-            "HTTP/1.1 403 Forbidden\r\n"
-            "Content-Type: text/html\r\n"
-            "Connection: close\r\n\r\n"
-            "<html><body><h1>403 Forbidden</h1></body></html>"
-        )
-        connectionSocket.send(response.encode())
-        connectionSocket.close()
-        continue
-
-    # File existence check
-    if os.path.exists(filename):
-        with open(filename, 'rb') as f:
-            body = f.read()
-        response = (
-            "HTTP/1.1 200 OK\r\n"
-            f"Content-Length: {len(body)}\r\n"
-            "Content-Type: text/html\r\n"
-            f"Last-Modified: {formatdate(os.path.getmtime(filename), usegmt=True)}\r\n"
-            "Connection: close\r\n\r\n"
-        ).encode() + body
-        print("Status code: 200")
+    # Handle the 404 case
     else:
-        print("Status code: 404")
         response = (
             "HTTP/1.1 404 Not Found\r\n"
             "Content-Type: text/html\r\n"
-            "Connection: close\r\n\r\n"
-            "<html><body><h1>404 Not Found</h1></body></html>"
+            "\r\n"
+            "<html><body><h1>404 Not Found</h1><p>The requested file does not exist on the server.</p></body></html>"
         ).encode()
 
     connectionSocket.send(response)
